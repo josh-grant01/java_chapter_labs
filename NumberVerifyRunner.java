a6cd590742ff1bc49f6c8add55e866cc1d51cab7
samhollenbeck <sam@hollenbecks.com>
2016-11-02 12:19:19 -0500

    Finished 5.2

--- NumberVerifyRunner.java
+++ NumberVerifyRunner.java
@@ -1,4 +1,4 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//Â© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date - 
 //Class -
@@ -11,9 +11,21 @@ public class NumberVerifyRunner
 {
 	public static void main ( String[] args )
 	{
-		//add in input
-		System.out.println("5 is odd :: " + NumberVerify.isOdd(5));
-		System.out.println("5 is even :: " + NumberVerify.isEven(5));
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Enter a whole number: ");
+		int num = keyboard.nextInt();
+		System.out.println(num + " is odd :: " + NumberVerify.isOdd(num));
+		System.out.println(num + " is even :: " + NumberVerify.isEven(num));
+		
+		System.out.println("Enter a whole number: ");
+		num = keyboard.nextInt();
+		System.out.println(num + " is odd :: " + NumberVerify.isOdd(num));
+		System.out.println(num + " is even :: " + NumberVerify.isEven(num));
+		
+		System.out.println("Enter a whole number: ");
+		num = keyboard.nextInt();
+		System.out.println(num + " is odd :: " + NumberVerify.isOdd(num));
+		System.out.println(num + " is even :: " + NumberVerify.isEven(num));
 		
 		//add in more test cases
 	}
