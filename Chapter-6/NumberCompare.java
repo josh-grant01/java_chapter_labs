69120912e826a4375b54196c2fe5335e62a80516
samhollenbeck <sam@hollenbecks.com>
2016-11-15 12:13:37 -0600

    Finished 6.1

--- Chapter-6/NumberCompare.java
+++ Chapter-6/NumberCompare.java
@@ -1,4 +1,4 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//Â© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class -
@@ -8,42 +8,24 @@ import static java.lang.System.*;
 
 public class NumberCompare
 {
-	// instance variables one and two are declared
-	// these variables (aka attributes, properties, data elements, fields, etc.)
-	//    hold onto the data (values) permanently	
-	// ALL methods inside this class have access to these instance variables
-
 	private int one, two;
 	
-	
-	// default constructor (no parameters)
-	// assign default values (0) to instance variables one and two
 	public NumberCompare()
 	{
-		
+		one = 0;
+		two = 0;
 	}
-
-	// initializer constructor 
-	// receives beginning values for our instance variables one and two
-	// n1 and n2 are created and receive values 
 	public NumberCompare(int n1, int n2)
 	{
+		setNums(n1,n2);
+	}
 
-
-	} // n1 and n2 are destroyed
-
-
-	// setter or modifier method setNums 
-	// receives new values for our instance variables one and two
-	// n1 and n2 are created and receive new values 
 	public void setNums(int n1, int n2)
 	{
-
-
-	} // n1 and n2 are destroyed
+		one = n1;
+		two = n2;
+	} 
 	
-
-	// returns the largest number 
 	public int getLargest()
 	{
 		if(one>two)
@@ -55,32 +37,14 @@ public class NumberCompare
 
 	public int getSmallest()
 	{
-		// add code here to return the smallest number
-		// between instance variables one and two
-		// DO NOT USE n1 and n2, they don't exist
-		
-		
-		
-		return 0;  // replace the 0
+		if (one<two) 
+			return one;
+		else
+			return two; 
 	}
 
-
-	// the toString() method should return one -- two
-	// where one and two are the values of your instance variables 
 	public String toString()
 	{
-		// this method should return the values of the instance variables
-		// in the form of   
-		// one -- two  
-		// where one should be replaced with
-		// the value of instance variable one and two should be replaced with
-		// the value of instance variable two
-		
-		// For example:  
-		// if instance variable one stores the value of 5
-		// and instance variable two stores the value of 6
-		// you would return the String   5 -- 6
-		
-		return "";  // replace the ""
+		return one + " -- " + two;
 	}
 }
