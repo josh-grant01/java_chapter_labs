f001157b684538eaaf8248a3a231d94ca8dadb69
samhollenbeck <sam@hollenbecks.com>
2016-11-18 12:14:01 -0600

    Finished 6.2

--- Chapter-6/StringEquality.java
+++ Chapter-6/StringEquality.java
@@ -1,4 +1,4 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//Â© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class -
@@ -8,29 +8,19 @@ import static java.lang.System.*;
 
 public class StringEquality
 {
-	// instance variables wordOne and wordTwo are declared
-	// these variables (aka attributes, properties, data elements, fields, etc.)
-	//    hold onto the data (values) permanently
-	// ALL methods inside this class have access to these instance variables
-
 	private String wordOne, wordTwo;
-
 	
-	// default constructor (no parameters)
-	// assign default values "" to instance variables wordOne and wordTwo	
 	public StringEquality()
 	{
-		
+		wordOne = "";
+		wordTwo = "";	
 		
 	}
-
-
-
 	// initializer constructor 
 	// receives beginning values for our instance variables wordOne and wordTwo
 	public StringEquality(String one, String two)
 	{
-		
+		setWords(one, two);
 		
 	}
 
@@ -40,29 +30,15 @@ public class StringEquality
 	// receives new values for our instance variables wordOne and wordTwo
 	public void setWords(String one, String two)
 	{
-		
-		
-		
+		wordOne = one;
+		wordTwo = two;
 	}
-
-
-	// this method returns the value true if
-	//    wordOne is equal to wordTwo
-	// DO NOT USE ==
-	// == is used for comparison of primitive types
-	// In order to compare string objects, call the equals() method
-	// For example:
-	// if (??????.equals(?????))
+	
 	public boolean checkEquality( )
 	{
-		// ***** DO NOT USE System.out.println() in this method
-		// a return method should generally NEVER use a System.out.println()
-		// it should find the return value and return it!!!!!!!!!!!!!!!!!!!!
-		 
-		
-		
-		
-		return false;
+		if(wordOne.equals(wordTwo))
+			return true;
+		return false; 	 
 	}
 
 
@@ -71,18 +47,8 @@ public class StringEquality
 	//    info about the instance variables
 	public String toString()
 	{
-		// ***** DO NOT USE System.out.println() in this method
-		// a return method should generally NEVER use a System.out.println()
-		// it should find the return value and return it!!!!!!!!!!!!!!!!!!!!
-		 
-
-
-		// ****** change me
-		// you should return does have or does NOT have
-		// you should probably think about using an if else statement
-		// you should probably call the method above
-		
-		
+		if(checkEquality())
+			return wordOne + " has the same letters as " + wordTwo + "\n";
 		return wordOne + " does NOT have the same letters as " + wordTwo + "\n";
 	}
 }
