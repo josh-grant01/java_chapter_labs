b1b50b3acd3dbe748392553275d854d383e181c5
samhollenbeck <sam@hollenbecks.com>
2017-01-04 12:06:21 -0600

    Finished 7.1

--- Chapter-7/Grade.java
+++ Chapter-7/Grade.java
@@ -1,4 +1,4 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//Â© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class -
@@ -11,73 +11,43 @@ public class Grade
 	// instance variable (or attribute, property, field)
 	private int numGrade;
 
-
-	// default constructor (SAME NAME AS THE CLASS)
-	//    to set your instance variable
-	//    to a beginning default value
 	public Grade()
 	{
-		
+		numGrade = 0;	
 	}
-
-
-	// initializer constructor (SAME NAME AS THE CLASS)
-	//    to set your instance variable
-	//    to a beginning value
-	// when another class calls our constructor, a
-	// temporary variable grade is created and
-	// receives a new grade value to store in the
-	// instance variable numGrade
+	
 	public Grade(int grade)
 	{
-		
+		setGrade(grade);	
 	}
 
-
-	// modifier or setter method 
-	//    to modify or set your instance variable
-	//    to a new value
-	// temporary variable grade is created and
-	// receives a new grade to store in your
-	// instance variable numGrade
 	public void setGrade(int grade)
 	{
-		
+		numGrade= grade;	
 	}
 
-
-	// DO NOT USE out.println or out.print statements
-	// in this method!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	//     the purpose of this method is to find the
-	//     letter grade and return it
-	// NOTE: you return the letter grade as a String
 	public String getLetterGrade( )
 	{
-		String letGrade="";
+			
+		String letGrade = "";
 		
-		// DO NOT USE out.println or out.print statements
-		// in this method!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		
-		// use an if else if else if ... statement
-		//    in order to find the letter grade
-		
-		// if (numGrade >= 90)
-		//     letGrade = "A";
-		// else if (numGrade >= 80)
-		//     letGrade = ???;
-		
-		
-		
-		
-		
-		
+		if(numGrade >= 90)
+			letGrade = "A";
+		else if (numGrade < 90 && numGrade >= 80)
+			letGrade = "B";
+		else if (numGrade < 80 && numGrade >= 70)
+			letGrade = "C";
+		else if (numGrade < 70 && numGrade >= 60)
+			letGrade = "D";
+		else
+			letGrade = "F";
+			
 		return letGrade;
 	}
 
-
-	// this method has been done for you
 	public String toString()
 	{
+		getLetterGrade();
 		return numGrade + " is a " + getLetterGrade();
 	}
 }
